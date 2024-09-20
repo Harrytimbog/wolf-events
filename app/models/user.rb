ce94a06daa421c72e823e2fc873fc68c41af54ef
Timilehin Arigbede <harriemannie@gmail.com>
2024-09-20 21:31:31 +0100

    Initial Commit + Add username, is_admin and roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  enum role: {viewer: "Viewer", chef: "Chef"}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :role, presence: true, inclusion: { in: roles.keys }
+  
 end
